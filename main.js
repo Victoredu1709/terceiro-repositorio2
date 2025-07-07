ea365ce9ac1739827c1a65a916f62281c869f2a2
Vctr-777 <125025169+Victoredu1709@users.noreply.github.com>
2025-07-07 08:24:26 -0300

    07/07

--- main.js
+++ main.js
@@ -15,54 +15,55 @@ botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho() {
     if (tamanhoSenha > 1) {
-    tamanhoSenha--;
+        tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
-geraSenha();
+    geraSenha();
 }
 
 function aumentaTamanho() {
     if (tamanhoSenha < 20){
-    tamanhoSenha++;
+        tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
-geraSenha();
+    geraSenha();
 }
+
 
 geraSenha();
 
 function geraSenha() {
-     let alfabeto = '';
-     if (checkbox[0].checked) {
-     alfabeto = alfabeto + letrasMaiusculas; 
-     }
-     if (checkbox[1].checked) {
-     alfabeto = alfabeto + letrasMinusculas; 
-     }
-     if (checkbox[2].checked) {
-     alfabeto = alfabeto + numeros; 
-     }
-     if (checkbox[3].checked) {
-     alfabeto = alfabeto + simbolos; 
-     }
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
 
-    let senha = '' ; 
+    let senha = '' ;
     for(let i=0; i < tamanhoSenha; i++) {
-    let numeroAleatorio = Math.random()*alfabeto.length;
-    numeroAleatorio = Math.floor(numeroAleatorio);
-senha = senha + alfabeto [numeroAleatorio];
-}
-campoSenha.value = senha;
+        let numeroAleatorio = Math.random()*alfabeto.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + alfabeto[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+    classificaSenha();
 }
 
-function classificaSenha(){
+function classificaSenha() {
     forcaSenha.classList.remove('fraca', 'media', 'forte');
-    if (tamanhoSenha>11) {
+    if (tamanhoSenha>11){
         forcaSenha.classList.add('forte');
-    } else if (tamanhoSenha>5 && tamanhoSenha<12); {
+    } else if (tamanhoSenha>5 && tamanhoSenha<12) {
         forcaSenha.classList.add('media');
-    } else if (tamanhoSenha<=5; 
-        forcaSenha.classList.add('fraca'); { 
-    } else if (tamanhoSenha>5 && tamanhoSenha<12); { 
-  }
+    } else if (tamanhoSenha<=5){
+        forcaSenha.classList.add('fraca');
+    }
 }
